735bca2f609f777ddbfe70fe81a41df543f2d26e
BigRedS <ialoneambest@gmail.com>
2011-10-26 23:41:00 +0100

    It works

--- bin/pissabout.pl
+++ bin/pissabout.pl
@@ -1,2 +1,21 @@
-#! /usr/bin/perl;
+#! /usr/bin/perl
 
+use strict;
+use lib '/home/avi/bin/onapp/lib/';
+use OnApp::API;
+
+open(my $f, "<", "/home/avi/.onapp_credentials") or die "Error getting credentials";
+my ($email, $key) = (<$f>);
+close($f);
+
+chomp($email);
+chomp($key);
+
+my $vm = OnApp::API->new(
+	api_email => $email,
+	 api_key  => $key,
+	api_url   => "cloudbase.us.positive-internet.com",
+);
+
+my $loadacrap = $vm->getUsers;
+print $loadacrap;
--- lib/OnApp/API.pm
+++ lib/OnApp/API.pm
@@ -4,8 +4,65 @@ package OnApp::API;
 
 use strict;
 use 5.010;
+use Carp;
+use LWP::UserAgent;
+use JSON;
 
 my $VERSION;
 $VERSION=0.20111026;
 
+sub new{
+	my $class = shift;
+	my %params = @_;
+	my $self = {};
 
+	my($apiUser,$apiPass);
+	if(exists($params{'api_email'})){
+		$apiUser = $params{'api_email'};
+		$apiPass = $params{'api_key'};
+	}elsif(exists($params{'api_user'})){
+		$apiUser = $params{'api_user'};
+		$apiPass = $params{'api_pass'};
+	}else{
+		Carp::croak("Neither 'api_email' nor 'api_user' passed to constructor");
+	}
+
+	my $apiURL;
+	if(exists($params{'api_url'})){
+		$apiURL = $params{'api_url'};
+	}else{
+		Carp::croak("'api_url' not passed to constructor");
+	}
+
+	my $apiUrlPort;
+	if(exists($params{'api_url_port'})){
+		$apiUrlPort = $params{'api_url_port'};
+	}else{
+		$apiUrlPort = "80";
+	}
+
+	my $apiRealm = "OnApp API Authentication";
+	my $headers = HTTP::Headers->new();
+	$headers->header('accept' => 'application/json');
+	$headers->header('content-type' => 'application/json');
+
+	
+	my $userAgent = LWP::UserAgent->new(
+	        default_headers => $headers,
+	);
+	my $apiLocation = $apiURL.":".$apiUrlPort;
+	$userAgent->credentials($apiLocation, $apiRealm, $apiUser, $apiPass);
+	$self->{'_userAgent'} = $userAgent;
+	$self->{'_url'} = "http://$apiURL";
+
+	bless($self);
+	return($self); 
+}
+
+sub getUsers(){
+	my $self = shift;
+	my $url = $self->{'_url'}."/users.json";
+	my $response = $self->{_userAgent}->get($url);
+	return $response->as_string();
+}
+1;
