bf1245cd668570a06b8662cc1bec3337eb3d1493
BigRedS <ialoneambest@gmail.com>
2011-12-28 18:43:59 +0000

    Fixing git

---
+++ lib/OnApp/API.pm
@@ -0,0 +1,221 @@
+#! /usr/bin/perl  
+
+package OnApp::API;
+
+use strict;
+#use warnings;
+use 5.010;
+use Carp;
+use LWP::UserAgent;
+use JSON;
+use Data::Dumper;
+
+my $VERSION;
+$VERSION=0.20111102;
+
+sub new{
+	my $class = shift;
+	my %params = @_;
+	my $self = {};
+
+	my($apiUser,$apiPass);
+	if(exists($params{'api_email'})){
+		$apiUser = $params{'api_email'};
+		$apiPass = $params{'api_key'};
+	}elsif(exists($params{'api_user'})){
+		$apiUser = $params{'api_user'};
+		$apiPass = $params{'api_pass'};
+	}else{
+		Carp::croak("Neither 'api_email' nor 'api_user' passed to constructor");
+	}
+
+	my $apiURL;
+	if(exists($params{'api_url'})){
+		$apiURL = $params{'api_url'};
+	}else{
+		Carp::croak("'api_url' not passed to constructor");
+	}
+
+	my $apiUrlPort;
+	if(exists($params{'api_url_port'})){
+		$apiUrlPort = $params{'api_url_port'};
+	}else{
+		$apiUrlPort = "80";
+	}
+
+	my $apiRealm = "OnApp API Authentication";
+	my $headers = HTTP::Headers->new();
+	$headers->header('accept' => 'application/json');
+	$headers->header('content-type' => 'application/json');
+
+	
+	my $userAgent = LWP::UserAgent->new(
+	        default_headers => $headers,
+	);
+	my $apiLocation = $apiURL.":".$apiUrlPort;
+	$userAgent->credentials($apiLocation, $apiRealm, $apiUser, $apiPass);
+	$self->{'_userAgent'} = $userAgent;
+	$self->{'_url'} = "http://$apiURL";
+
+	bless($self);
+	return($self); 
+}
+
+sub getHash(){
+	my $self = shift;
+<<<<<<< HEAD
+	my %args = @_;
+	my $url = $self->{'_url'}."/users.json";
+	print "<$url>";
+	my $reference = $self->_getRef($url);
+	unless(%args){
+		return $reference;
+	}
+		
+}
+
+sub getVMs(){
+	my $self = shift;
+	my %args = @_;
+	my $url = $self->_getUrl("get", "vms");
+	my $reference = $self->_getRef($url);
+	my @machines = @{$reference};
+	my %machine;
+	my %data;
+	foreach my $m (@machines){
+		%machine = %{$m};
+		foreach my $virtual_machine (keys(%machine)){
+			my $hostname = $machine{$virtual_machine}{'hostname'};
+			$data{$hostname} = $machine{$virtual_machine};
+		}
+	}
+	my $return = \%data;
+	if( exists($args{'andFilter'}) || exists($args{'orFilter'}) ){
+		$return = $self->_applyFilter(
+			data      => $return,
+			andFilter => $args{'andFilter'},
+#			orFilter  => $args{'orFilter'}
+		);
+	}
+	return $return;
+	
+}
+
+sub _applyFilter{
+	my $self = shift;
+	my %args = @_;
+	my $data = $args{'data'};
+
+#	if ( exists($args{'orFilter'}) ){
+#		say "Or Filtering";
+#		$data  = $self->_applyOrFilter(
+#			data	=> $data,
+#			filter	=> $args{'orFilter'},
+#		)
+#	}
+			
+	if ( exists($args{'andFilter'}) ){
+		say "And Filtering";
+		$data = $self->_applyAndFilter(
+			data	=> $data,
+			filter	=> $args{'andFilter'}
+		);
+	}
+
+	return $data
+}
+
+
+sub _applyAndFilter{
+	my $self = shift;
+	my %args = @_;
+	my %data = %{ $args{'data'} };
+	say "AND in the function";
+	say "AND still here";
+
+	my %filter = %{ $args{'filter'} };
+
+	return $args{'data'} unless $args{'filter'};
+	foreach my $element ( keys(%data) ){
+		foreach my $condKey ( keys(%filter) ){
+			my $condVal = $filter{$condKey};
+			if( exists($data{$element}{$condKey}) ){
+				delete( $data{$element} ) unless $data{$element}{$condKey} =~ /$condVal/i;
+			}else{
+			}
+		}
+	}
+	my $return = \%data;
+	return $return;
+}
+
+sub _applyOrFilter{
+	my $self = shift;
+	my %args = @_;
+	my %data = %{ $args{'data'} };
+	my %filter = %{ $args{'filter'} };
+
+	return $args{'data'} if !%filter;
+
+	my %keepData;
+	my %data;
+	foreach my $element ( keys(%data) ){
+		foreach my $condKey ( keys(%filter) ){
+			my $condVal = $filter{$condKey};
+			if( exists($data{$element}{$condKey}) ){
+				if( $data{$element}{$condKey} =~ /$condVal/i ){
+					say "OR keeping $element = $data{$element}";
+					$keepData{$element} = $data{$element}
+				}
+			}
+		}
+	}
+	my $return = \%keepData;
+	return $return;
+}
+
+sub _getRef{
+	my $self = shift;
+	my $url = shift;
+	my $response = $self->{_userAgent}->get($url);
+	my $body = $response->content;
+	my $hash = decode_json($body);
+	return $hash;
+}
+
+sub _getUrl{
+	my $self = shift;
+	my $getOrSet = shift;
+	my $what = shift;
+	my %uris = (
+		get => {
+			users	=> '/users.json',
+			vms	=> '/virtual_machines.json',
+		},
+	);
+	my $uri = $uris{$getOrSet}{$what};
+	my $url = $self->{'_url'}.$uri;
+	return $url;
+
+=======
+	my $url = shift;
+	my $response = $self->{_userAgent}->get($url);
+	my $json = $response->content;
+	my $data = from_json($json);
+	return $data;
+
+}
+
+sub getUsers(){
+	my $self = shift;
+	my $url = $self->{_url}."/users.json";
+	my $data = $self->getHash($url);
+	print Dumper($data);
+>>>>>>> fe9d66620fcbf55853fe0e1fb0c7ee9344dd130c
+}
+
+
+
+
+1;
+
