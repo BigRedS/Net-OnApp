a1c1d0e0c561b2c4829c2a18e5f44c1d1624bd7c
BigRedS <ialoneambest@gmail.com>
2011-12-30 14:39:57 +0000

    Stupid fucking capitalisation

--- lib/OnApp/API.pm
+++ lib/OnApp/API.pm
@@ -30,6 +30,40 @@ OnApp::API - Perlish interface to OnApp API
 
   my $hashOfVms = $vm->getVMs();
 
+  my $response = $vm->createVM(
+        cpus    => 1, 
+        cpu_shares => 2, 
+        Hostname => "avitest",
+        label => "avi testing",
+        primary_disk_size => 8, 
+        swap_disk_size => 1, 
+        rate_limit => 100,
+        admin_note => "do-wop",
+        Note => "doo-wop",
+        template_id => $templateID,
+  );
+
+
+Each of the C<create> methods returns a hashref containing some useful data, but
+no real information. Keys are:
+
+=over 4
+
+=item C<status_code> => HTTP numerical status code
+
+=item C<status> => HTTP standard description for the HTTP status code
+
+=item C<status_description> => OnApp's explanation of the HTTP status code.
+
+=item C<requested_url> => The URL requested
+
+=item C<json> => The JSON sent
+
+=item C<response_objct> => The response object as returned by the C<LWP::UserAgent> object 
+used to POST the data.
+
+=back
+
 =head1 CONSTRUCTOR AND STARTUP
 
 =head3 new()
@@ -81,7 +115,6 @@ sub new{
 	my $headers = HTTP::Headers->new();
 	$headers->header('accept' => 'application/json');
 	$headers->header('content-type' => 'application/json');
-#	$headers->header('content-type' => 'application/x-www-form-urlencoded');
 
 	
 	my $userAgent = LWP::UserAgent->new(
@@ -119,17 +152,65 @@ sub getUsers{
 
 =head3 createUser()
 
+Creates a user based on the hashref it's passed. 
+
+Mandatory fields:
+
+=over 4
+
+=item C<email> => user’s email address
+
+=item C<first_name> => user’s first name
+
+=item C<last_name> => user’s last name
+
+=item C<login> => login of the user. It can consist of 4-40 characters, letters [A-Za-z], digits [0-9], dash [ - ], lower dash [ _ ], [@]. You can use both lower- and uppercase letters
+
+=item C<password> => user’s password. (min – 6 characters)
+
+=item C<password_confirmation> => confirmation of the password (retype the password)
+
+=back
+
+If C<password_confirmaton> is not passed, it is automatically set to the value of C<password>. If it is 
+passed and is empty, it will be passed to OnApp as such and you'll probably get an error in return.
+
+Optional fields:
+
+=over4
+
+=item C<role> => assigns a role to a user
+
+=item C<time_zone> => time zone of the user. Set by default
+
+=item C<locale> => local of the user. Set by default
+
+=item C<status> => user’s status (active, suspended, etc)
+
+=item C<billing_plan_id> => set by default, if not selected
+
+=item C<role_ids> => ID of the role, assigned to the user
+
+=item C<user_group_id> => ID of the group, to which the user is attached
+
+=item C<suspend_after_hours> => time in hours, after which the user will be suspended
+
+=item C<suspend_at> => time in [YYYY][MM][DD] T[hh][mm][ss]Z format, when the user will be suspended
+
+=back
+
 =cut
 
 sub createUser{
 	my $self = shift;
 	my %args =@_;
 	my $url = $self->_getUrl("get", "vms");
-	# set some defaults:
+
+	$args{'password_confirmation'} = $args{'password'} unless exists($args{'password_confirmation'});
+	
 	my %params;
 	%params = (%params, %args);
 	my @requiredParams = qw/email first_name last_name login password password_confirmation/;
-
 	foreach my $param (@requiredParams){
 		Carp::croak "Paramater '$param' not passed to createVM " unless(exists($params{$param}));
 	}
@@ -137,8 +218,6 @@ sub createUser{
 	my $url = $self->_getUrl("set", "users");
 	my $response = $self->_postJson(\%params, $url, "user");
 
- #$self->_postJson( $hashref, $containerName, $url);
-#	print $response->as_string;
 	return $response;
 	return;	
 
@@ -167,23 +246,63 @@ sub getVMs(){
 }
 
 =head3 createVM();
-> *cpus	 		Number of CPUs assigned to the VM.
-> *cpu_shares		Set CPU priority for this VM.
-> *Hostname		Set the host name for this VM.
-> *label		User-friendly VM description.
-> *primary_disk_size	Set the disk space for this VM. in GB.
-> *swap_disk_size	Set swap space. There is no swap disk for Windows-based VMs.
-> primary_network_id	The ID of the primary network. Optional parameter.
-> required_automatic_backup	Set 1 if you need automatic backups.
-> rate_limit		Set max port speed. Optional parameter: if none set, the system sets port speed to unlimited.
-> *required_ip_address_assignment	Set 1 if you wish the system to assign an IP automatically
-> *required_virtual_machine_build	Set 1 to build VM automatically
-> admin_note		Enter a brief comment for the VM. Optional parameter.
-> Note			A brief comment a user can add to a VM.
-> *template_id	 	The ID of a template from which a VM should be built
-> hypervisor_group_id	The ID of the hypervisor zone in which the VM will be created. Optional: if no hypervisor zone is set, the VM will  be built in any available hypervisor zone.
-> hypervisor_id		The ID of a hypervisor where the VM will be built.
-> initial_root_password Root password. [\w\-\_]{6,32} created if not supplied
+
+Creates a virtual machine according to the parameters in the hashref passed as an argument. Items
+with asterisks are mandatory, wording below is essentially verbatim from the API docs.
+
+There's some stupid capitalisation going on in the docs - everything must be lower case.
+
+I've set three defaults:
+
+ required_automatic_backup => "0",
+ required_ip_address_assignment => "1",
+ required_virtual_machine_build => "1",
+
+Mandatory fields:
+
+=over4
+
+=item C<cpus> => Number of CPUs assigned to the VM.
+
+=item C<cpu_shares> => Set CPU priority for this VM.
+
+=item C<hostname> => Set the host name for this VM.
+
+=item C<label> => User-friendly VM description.
+
+=item C<primary_disk_size> => Set the disk space for this VM. in GB.
+
+=item C<swap_disk_size> => Set swap space. There is no swap disk for Windows-based VMs.
+
+=item C<required_ip_address_assignment> => Set 1 if you wish the system to assign an IP automatically
+
+=item C<required_virtual_machine_build> => Set 1 to build VM automatically
+
+=item C<template_id> => The ID of a template from which a VM should be built
+
+=back
+
+Optional fields:
+
+=over 4
+
+=item C<primary_network_id> => The ID of the primary network. Optional parameter.
+
+=item C<required_automatic_backup> => Set 1 if you need automatic backups.
+
+=item C<rate_limit> => Set max port speed. Optional parameter: if none set, the system sets port speed to unlimited.
+
+=item C<admin_note> => Enter a brief comment for the VM. Optional parameter.
+
+=item C<note> => A brief comment a user can add to a VM.
+
+=item C<hypervisor_group_id> => The ID of the hypervisor zone in which the VM will be created. Optional: if no hypervisor zone is set, the VM will  be built in any available hypervisor zone.
+
+=item C<hypervisor_id> => The ID of a hypervisor where the VM will be built.
+
+=item C<initial_root_password> => Root password. [\w\-\_]{6,32} created if not supplied
+
+=back
 
 =cut
 
@@ -199,23 +318,15 @@ sub createVM(){
 	);
 
 	%params = (%params, %args);
-	my @requiredParams = qw/cpus cpu_shares Hostname label primary_disk_size swap_disk_size required_ip_address_assignment required_virtual_machine_build template_id/;
+	my @requiredParams = qw/cpus memory cpu_shares hostname label primary_disk_size swap_disk_size required_ip_address_assignment required_virtual_machine_build template_id/;
 	foreach my $param (@requiredParams){
 		Carp::croak "Paramater '$param' not passed to createVM " unless(exists($params{$param}));
 	}
 
-	my $json = $self->_makeJson(\%params);
-	my $json = '{"virtual_machine":'.$json.'}';
-
-#	print $json;
-#	say "x " x20;
 	my $url = $self->_getUrl("set", "vms");
-#	my $response = $self->_postJson(\%params, $url);
-	my $output = $self->_postJson(\%params, $url);
+	my $response = $self->_postJson(\%params, $url, "virtual_machine");
 
-	my $status = $output->status_description;
-
-	return $status;
+	return $response;
 
 }
 
@@ -241,7 +352,7 @@ sub getTemplates{
 
 Don't use these.
 
-=head2 _tidyData()
+=head3 _tidyData()
 
 Makes the data structure more sane. The API, when passed through C<JSON::decode_json()>, 
 produces an array of hashrefs. Each of those hashes itself contains a single hashref
@@ -279,16 +390,6 @@ sub _tidyData{
 	return $return;
 }
 
-sub _untidyData{
-	my $self = shift;
-	my $ref = shift;
-	my $extraName = shift;
-	my @array = {
-		$extraName => $ref
-	};
-	return \@array;
-}
-
 #	if( exists($args{'andFilter'}) || exists($args{'orFilter'}) ){
 #		$return = $self->_applyFilter(
 #			data      => $return,
@@ -297,7 +398,7 @@ sub _untidyData{
 #	}
 #	return $return;
 
-=head2 _applyFilter(), _applyAndFilter(), _applyOrFilter()
+=head3 _applyFilter(), _applyAndFilter(), _applyOrFilter()
 
 This is currently not used. It seems a bit pointless trying to provide a
 generic filter here when it might as well be done outside of the module
@@ -377,7 +478,7 @@ sub _applyOrFilter{
 	return $return;
 }
 
-=head2 _getRef()
+=head3 _getRef()
 
 Given a full URL (as returned by C<_getURL> ), returns a hashref containing
 The data.
@@ -411,7 +512,7 @@ sub _postJson{
 	my $containerName = shift;
 
 	my $json = $self->_makeJson($ref);
-	my $json = '{"'.$containerName.'":'.$json.'}';
+	$json = '{"'.$containerName.'":'.$json.'}';
 
 	my $req = HTTP::Request->new(POST => $url);
 	$req->content_type('application/json');
@@ -436,7 +537,23 @@ sub _postJson{
  $self->_explainStatusCode($HTTPStatusCode);
 
 Given an HTTP status code, returns OnApp's explanation of why it would return it. Text is from
-the FAQ of the API guide.
+the FAQ of the API guide:
+
+=over4
+
+=item  200 => "The request completed successfully",
+
+=item  201 => "Scheduled The request has been accepted and scheduled for processing",
+
+=item  403 => "Forbidden The request is correct, but could not be processed.",
+
+=item  404 => "The requested URL is incorrect or the resource does not exist. For example, if you request to delete a user with ID {5}, but there is no such a user in the cloud, you will get a 404 error.",
+
+=item  422 => "The sent parameters are erroneous.",
+
+=item  500 => "An error occurred. Please contact support.",
+
+=back
 
 =cut
 
@@ -468,7 +585,7 @@ sub _makeJson{
 	return $json;
 }
 
-=head2 _getUrl()
+=head3 _getUrl()
 Used to create URLs from easy-to-remember names. For example, to get
 the correct URL for the API to return a list of users, you can do
 
@@ -507,7 +624,7 @@ sub _getUrl{
 }
 
 
-=head2 _getHash()
+=head3 _getHash()
 
 Deprecated already, in favour of C<_getRef>. 'cause references are cool.
 
