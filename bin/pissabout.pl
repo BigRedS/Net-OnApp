5e93a08c11f9212f8a4a691963e2ecb8a6771655
BigRedS <ialoneambest@gmail.com>
2011-12-30 09:12:02 +0000

    possibly making vms

--- bin/pissabout.pl
+++ bin/pissabout.pl
@@ -15,22 +15,29 @@ chomp($key);
 
 my $vm = OnApp::API->new(
 	api_email => $email,
-	 api_key  => $key,
+        api_key   => $key,
 	api_url   => "cloudbase.us.positive-internet.com",
 );
 
-my $loadacrap = $vm->getVMs(
-	andFilter => {
-		hostname => "gin",
-	},
-);
-
-#print Dumper($loadacrap);
-my %hash = %{$loadacrap};
-foreach(keys(%hash)){
-	say;
+my $t = $vm->getTemplates;
+my %templates = %{ $t };
+#print Dumper(%templates);
+my $templateID;
+foreach(keys(%templates)){
+	if ($templates{$_}{label} =~ /^Debian 6\.0 x64$/i){
+		$templateID = $_;
+	}
 }
 
-
-
-
+$vm->createVM(
+	cpus	=> 1,
+	cpu_shares => 2,
+	Hostname => "avitest",
+	label => "avi testing",
+	primary_disk_size => 8,
+	swap_disk_size => 1,
+	rate_limit => 100,
+	admin_note => "do-wop",
+	Note => "doo-wop",
+	template_id => $templateID,
+);
